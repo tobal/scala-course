857ce7c47c5c770d3c9da86ab0386dda2dfe66e9
Tobal <tobal17@gmail.com>
2016-07-08 15:04:43 +0200

    recfun: project added
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,28 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+    def pascal(c: Int, r: Int): Int = ???
+
+  /**
+   * Exercise 2
+   */
+    def balance(chars: List[Char]): Boolean = {
+        def func(chars: List[Char], deviance: Int): Boolean = ???
+  
+  /**
+   * Exercise 3
+   */
+    def countChange(money: Int, coins: List[Int]): Int = ???
+  }
