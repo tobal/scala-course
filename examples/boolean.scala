b1d4e5ff4c623e379d2032295be6073a225dda4d
Tobal <tobal17@gmail.com>
2016-07-27 15:56:01 +0200

    examples: add boolean example
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ examples/boolean.scala
@@ -0,0 +1,13 @@
+
+abstract class Boolean {
+    def ifThenElse[T](t: => T, e: => T):T
+
+    def && (x: => Boolean): Boolean = ifThenElse(x, false)
+    def || (x: => Boolean): Boolean = ifThenElse(true, x)
+    def unary_!: Boolean = ifThenElse(false, true)
+
+    def == (x: Boolean): Boolean = ifThenElse(x, x.unary_!)
+    def != (x: Boolean): Boolean = ifThenElse(x.unary_!, x)
+
+    def < (x: Boolean): Boolean = ifThenElse(false, x)
+}
