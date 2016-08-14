31854a756de25965345546515651c1017d0b9b95
Tobal <tobal17@gmail.com>
2016-08-14 13:23:55 +0200

    examples.list: fix
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/list.scala
+++ examples/list.scala
@@ -15,13 +15,13 @@ class Nil[T] extends List[T] {
     def tail: Nothing = throw new NoSuchElementException("Nil.tail")
 }
 
-def nth[T](idx: Int, l: List[T]) {
-    if (l.isEmpty) throw IndexOutOfBoundsException
-    else if (idx == 0) l.head
-    else nth(idx - 1, l.tail)
-}
-
 object List {
     // List(1, 2) = List.apply(1, 2)
     def apply[T](x1: T, x2: T): List[T] = new Cons(x1, new Cons(x2, new Nil))
+
+    def nth[T](idx: Int, l: List[T]) {
+        if (l.isEmpty) throw IndexOutOfBoundsException
+        else if (idx == 0) l.head
+        else nth(idx - 1, l.tail)
+    }
 }
