9c019ac95605e6acfaf337a8318800b71938785b
Tobal <tobal17@gmail.com>
2016-08-18 22:53:07 +0200

    forcomp: add combinations impl
    Signed-off-by: Tobal <tobal17@gmail.com>

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -81,7 +81,18 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+    def combinations(occurrences: Occurrences): List[Occurrences] = occurrences match {
+        case List() => List()
+        case head :: tail => {
+            val tailCombinations = combinations(tail)
+            tailCombinations ++ (
+                for {
+                    o <- tailCombinations
+                    i <- 1 to head._2
+                } yield (head._1, i) :: o
+            )
+        }
+    }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
