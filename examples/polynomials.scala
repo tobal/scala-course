16b30e0fabdecfb2ed6083229a8cb601b5f87b67
Tobal <tobal17@gmail.com>
2016-08-22 19:53:58 +0200

    examples.poly: use variable length argument list
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/polynomials.scala
+++ examples/polynomials.scala
@@ -1,6 +1,8 @@
 
 object polynomials {
     class Poly(terms0: Map[Int, Double]) {
+        def this(bindings: (Int, Double)*) = this(bindings.toMap)
+
         val terms = terms0 withDefaultValue 0.0
 
         def + (other: Poly) = new Poly(terms ++ (other.terms map adjust))
@@ -14,7 +16,7 @@ object polynomials {
             (for ((exp, coeff) <- terms.toList.sorted.reverse) yield coeff + "x^" + exp) mkString " + "
     }
 
-    val p1 = new Poly(Map(1 -> 2.0, 3 -> 4.0, 5 -> 6.2))
-    val p2 = new Poly(Map(0 -> 3.0, 3 -> 7.0))
+    val p1 = new Poly(1 -> 2.0, 3 -> 4.0, 5 -> 6.2)
+    val p2 = new Poly(0 -> 3.0, 3 -> 7.0)
     p1 + p2
 }
