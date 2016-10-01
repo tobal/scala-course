66c50805d5712366c8a2130ba1ba9f4af93ded7e
Tobal <tobal17@gmail.com>
2016-10-01 11:30:14 +0200

    streams: add startBlock implementation
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-program-design/streams/src/main/scala/streams/GameDef.scala
+++ functional-program-design/streams/src/main/scala/streams/GameDef.scala
@@ -84,7 +84,7 @@ trait GameDef {
    * This function returns the block at the start position of
    * the game.
    */
-  def startBlock: Block = ???
+  def startBlock: Block = new Block(startPos, startPos)
 
 
   /**
