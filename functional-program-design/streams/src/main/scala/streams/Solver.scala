b60cf01a7c94152ffe5f381a5ac7bda88a9ef5fe
Tobal <tobal17@gmail.com>
2016-11-02 23:12:35 +0100

    streams: add implementation for pathsToGoal
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-program-design/streams/src/main/scala/streams/Solver.scala
+++ functional-program-design/streams/src/main/scala/streams/Solver.scala
@@ -88,7 +88,7 @@ trait Solver extends GameDef {
    * Returns a stream of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: Stream[(Block, List[Move])] = ???
+  lazy val pathsToGoal: Stream[(Block, List[Move])] = pathsFromStart.filter((tuple: (Block, List[Move])) => done(tuple._1))
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
