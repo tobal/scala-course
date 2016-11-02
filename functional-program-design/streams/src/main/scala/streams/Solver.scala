26bbd46eaa01e2dea297054f99c8b2e1e447038e
Tobal <tobal17@gmail.com>
2016-11-02 23:01:32 +0100

    streams: add implementation for from
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-program-design/streams/src/main/scala/streams/Solver.scala
+++ functional-program-design/streams/src/main/scala/streams/Solver.scala
@@ -67,8 +67,14 @@ trait Solver extends GameDef {
    * of different paths - the implementation should naturally
    * construct the correctly sorted stream.
    */
-  def from(initial: Stream[(Block, List[Move])],
-           explored: Set[Block]): Stream[(Block, List[Move])] = ???
+    def from(initial: Stream[(Block, List[Move])], explored: Set[Block]): Stream[(Block, List[Move])] = {
+      val more = for {
+        (block, path) <- initial
+        neighbor <- newNeighborsOnly(neighborsWithHistory(block, path), explored)
+      } yield neighbor
+      initial append from(more, explored ++ (more map (_._1)))
+    }
+
 
   /**
    * The stream of all paths that begin at the starting block.
