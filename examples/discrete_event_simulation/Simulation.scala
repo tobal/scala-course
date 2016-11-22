5145b6141a72b6cf13a4e52a886a10e458b6aee4
Tobal <tobal17@gmail.com>
2016-11-22 18:34:49 +0100

    examples.discrete_event_simulation: finalize Simulation class
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/discrete_event_simulation/Simulation.scala
+++ examples/discrete_event_simulation/Simulation.scala
@@ -1,6 +1,39 @@
 
-trait Simulation {
-  def currentTime = ???
-  def afterDelay(delay: Int)(block: => Unit): Unit = ???
-  def run(): Unit = ???
+abstract class Simulation {
+
+  type Action = () => Unit
+
+  case class Event(time: Int, action: Action)
+
+  private var curtime = 0
+  def currentTime: Int = curtime
+
+  private type Agenda = List[Event]
+  private var agenda: Agenda = List()
+
+  private def insert(ag: Agenda, item: Event): Agenda = ag match {
+    case first :: rest if first.time <= item.time => first :: insert(rest, item)
+    case _ => item :: ag
+  }
+
+  def afterDelay(delay: Int)(block: => Unit): Unit = {
+    val item = Event(currentTime + delay, () => block)
+    agenda = insert(agenda, item)
+  }
+
+  def run(): Unit = {
+    afterDelay(0) {
+      println("*** simulation started, time = " + currentTime + " ***")
+    }
+    loop()
+  }
+
+  private def loop(): Unit = agenda match {
+    case first :: rest =>
+      agenda = rest
+      curtime = first.time
+      first.action()
+      loop()
+    case Nil =>
+  }
 }
