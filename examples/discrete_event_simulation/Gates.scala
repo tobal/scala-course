5f364e5ec8f92d6ba34681f1c1c55070681e8eb0
Tobal <tobal17@gmail.com>
2016-11-22 17:13:22 +0100

    examples.discrete_event_simulation: add andGate function
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/discrete_event_simulation/Gates.scala
+++ examples/discrete_event_simulation/Gates.scala
@@ -17,7 +17,17 @@ class Wire {
 def inverter(input: Wire, output: Wire): Unit = {
   def invertAction(): Unit = {
     val inputSig = input.getSignal
-    afterDelay(InverterDelay) { output setSignel !inputSig }
+    afterDelay(InverterDelay) { output setSignal !inputSig }
   }
   input addAction invertAction
+}
+
+def andGate(in1: Wire, in2: Wire, output: Wire): Unit = {
+  def andAction(): Unit = {
+    val in1Sig = in1.getSignal
+    val in2Sig = in2.getSignal
+    afterDelay(AndGateDelay) { output setSignal (in1Sig & in2Sig) }
+  }
+  in1 addAction andAction
+  in2 addAction andAction
 }
