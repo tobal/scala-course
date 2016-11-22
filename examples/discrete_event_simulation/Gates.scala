e05c42552c907c2aaf0a9ff2c65df4a3b67756c4
Tobal <tobal17@gmail.com>
2016-11-22 18:06:49 +0100

    examples.discrete_event_simulation: add orGate function
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/discrete_event_simulation/Gates.scala
+++ examples/discrete_event_simulation/Gates.scala
@@ -31,3 +31,13 @@ def andGate(in1: Wire, in2: Wire, output: Wire): Unit = {
   in1 addAction andAction
   in2 addAction andAction
 }
+
+def orGate(in1: Wire, in2: Wire, output: Wire): Unit = {
+  def orAction(): Unit = {
+    val in1Sig = in1.getSignal
+    val in2Sig = in2.getSignal
+    afterDelay(OrGateDelay) { output setSignal (in1Sig | in2Sig) }
+  }
+  in1 addAction orAction
+  in2 addAction orAction
+}
