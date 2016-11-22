5b3c3bc0fe55f7ef6d071736e1f826ad436c86be
Tobal <tobal17@gmail.com>
2016-11-22 18:15:00 +0100

    examples.discrete_event_simulation: add fullAdder
    Signed-off-by: Tobal <tobal17@gmail.com>

--- examples/discrete_event_simulation/Circuits.scala
+++ examples/discrete_event_simulation/Circuits.scala
@@ -8,4 +8,11 @@ abstract class Circuits extends Gates {
     inverter(c, e)
     andGate(d, e, s)
   }
+
+  def fullAdder(a: Wire, b: Wire, cin: Wire, sum: Wire, cout: Wire) {
+    val s, c1, c2 = new Wire
+    halfAdder(a, cin, s, c1)
+    halfAdder(b, s, sum, c2)
+    orGate(c1, c2, cout)
+  }
 }
