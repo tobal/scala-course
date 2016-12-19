7d48d25e56e6b87ede6e87103281cf3934f84e2b
Tobal <tobal17@gmail.com>
2016-12-19 16:14:52 +0100

    quickcheck: add generator function
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-program-design/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ functional-program-design/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -9,12 +9,15 @@ import Prop._
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
-  lazy val genHeap: Gen[H] = ???
+  lazy val genHeap: Gen[H] = for {
+    i <- arbitrary[A]
+    h <- frequency((1, Gen.const(empty)), (9, genHeap))
+  } yield insert(i, h)
+
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
   property("gen1") = forAll { (h: H) =>
     val m = if (isEmpty(h)) 0 else findMin(h)
     findMin(insert(m, h)) == m
   }
-
 }
