b27ab25663fc906077863b26ec0bbde343bc540a
Tobal <tobal17@gmail.com>
2017-01-04 09:39:09 +0100

    examples: rewrite bankaccount to use signals
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ examples/bankaccount/Accounts.scala
@@ -0,0 +1,12 @@
+
+object accounts {
+  def consolidated(accts: List[BankAccount]): Signal[Int] =
+    Signal(accts.map(_.balance()).sum)
+
+  val a = new BankAccount()
+  val b = new BankAccount()
+  val c = consolidated(List(a, b))
+  c()
+  a deposit 20
+  c()
+}
--- examples/bankaccount/BankAccount.scala
+++ examples/bankaccount/BankAccount.scala
@@ -1,14 +1,16 @@
 
 class BankAccount {
-  private var balance = 0
+  val balance = Var(0)
 
   def deposit(amount: Int): Unit =
     if (amount > 0) {
-      balance = balance + amount
+      val b = balance()
+      balance() = b + amount
     }
 
   def withdraw(amount: Int): Unit =
-    if (0 < amount && amount <= balance) {
-      balance = balance - amount
+    if (0 < amount && amount <= balance()) {
+      val b = balance()
+      balance() = b - amount
     } else throw new Error("insufficient funds")
 }
