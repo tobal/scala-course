4991f42c2379b98f02fd53b05c57fef412bebab3
Tobal <tobal17@gmail.com>
2017-01-12 17:09:10 +0100

    calcualtor: root solver imlementation
    Signed-off-by: Tobal <tobal17@gmail.com>

--- functional-program-design/calculator/src/main/scala/calculator/Polynomial.scala
+++ functional-program-design/calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,13 +1,21 @@
 package calculator
 
+import Math.sqrt
+
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    val _b = b()
+    Signal(_b * _b - 4 * a() * c())
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    val square = sqrt(computeDelta(a, b, c)())
+    val _a = a()
+    val _b = b()
+    val first = (-_b + square) / (2 * _a)
+    val second = (-_b - square) / (2 * _a)
+    Signal(Set(first, second))
   }
 }
