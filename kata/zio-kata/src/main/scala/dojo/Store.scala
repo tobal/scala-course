edbc2c14998cec805bb9d0af8de90093da9654e1
Tobal <tobal17@gmail.com>
2021-04-26 16:16:35 +0200

    kata/zio-kata: add empty sbt project
    Signed-off-by: Tobal <tobal17@gmail.com>

---
+++ kata/zio-kata/src/main/scala/dojo/Store.scala
@@ -0,0 +1,3 @@
+package dojo
+
+object Store {}
---
+++ kata/zio-kata/src/test/scala/dojo/StoreSuite.scala
@@ -0,0 +1,9 @@
+package dojo
+
+import org.scalatest.Inside
+import org.scalatest.freespec.AnyFreeSpecLike
+import org.scalatest.matchers.should.Matchers
+
+class StoreSuite extends AnyFreeSpecLike with Matchers with Inside {
+  "init" in pending
+}
