57a16389a31395fcad98ba75c1a31eaf2a5e2fc3
Tobal <tobal17@gmail.com>
2021-04-27 21:38:36 +0200

    kata/zio-kata: add Store service with get function returning None
    Signed-off-by: Tobal <tobal17@gmail.com>

--- kata/zio-kata/src/main/scala/dojo/Store.scala
+++ kata/zio-kata/src/main/scala/dojo/Store.scala
@@ -1,3 +1,17 @@
 package dojo
+import zio.{Has, ZIO, ZLayer}
 
-object Store {}
+object Store {
+  type Store = Has[Service]
+
+  trait Service {
+    def get(key: String): ZIO[Any, Nothing, Option[Int]]
+  }
+
+  def get(key: String): ZIO[Store, Nothing, Option[Int]] =
+    ZIO.accessM[Store](_.get.get(key))
+
+  val Live: ZLayer[Any, Nothing, Store] = ZLayer.succeed(new Service {
+    override def get(key: String): ZIO[Any, Nothing, Option[Int]] = ZIO.none
+  })
+}
--- kata/zio-kata/src/test/scala/dojo/StoreSuite.scala
+++ kata/zio-kata/src/test/scala/dojo/StoreSuite.scala
@@ -1,9 +1,17 @@
 package dojo
 
+import dojo.Store.Store
 import org.scalatest.Inside
 import org.scalatest.freespec.AnyFreeSpecLike
 import org.scalatest.matchers.should.Matchers
+import zio.ZIO
+import zio.Runtime
 
 class StoreSuite extends AnyFreeSpecLike with Matchers with Inside {
-  "init" in pending
+  "should return None if key doesn't exist" in {
+    val prog: ZIO[Store, Nothing, Option[Int]] = Store.get("notExists")
+    val provided: ZIO[Any, Nothing, Option[Int]] = prog.provideLayer(Store.Live)
+    val default = Runtime.default
+    default.unsafeRun(provided) shouldBe None
+  }
 }
