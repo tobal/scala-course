21730b81e47471e6e6d9f4dfcfdb360a3663eecc
Tobal <tobal17@gmail.com>
2021-04-28 20:42:14 +0200

    kata/zio-kata: add set function to Store
    Signed-off-by: Tobal <tobal17@gmail.com>

--- kata/zio-kata/src/main/scala/dojo/Store.scala
+++ kata/zio-kata/src/main/scala/dojo/Store.scala
@@ -1,17 +1,24 @@
 package dojo
-import zio.{Has, ZIO, ZLayer}
+import zio.{Has, Ref, ZIO, ZLayer}
 
 object Store {
   type Store = Has[Service]
 
   trait Service {
     def get(key: String): ZIO[Any, Nothing, Option[Int]]
+    def set(key: String, value: Int): ZIO[Any, Nothing, Unit]
   }
 
   def get(key: String): ZIO[Store, Nothing, Option[Int]] =
     ZIO.accessM[Store](_.get.get(key))
 
-  val Live: ZLayer[Any, Nothing, Store] = ZLayer.succeed(new Service {
-    override def get(key: String): ZIO[Any, Nothing, Option[Int]] = ZIO.none
+  def set(key: String, value: Int): ZIO[Store, Nothing, Unit] =
+    ZIO.accessM[Store](_.get.set(key, value))
+
+  val live: ZLayer[Any, Nothing, Store] = ZLayer.fromEffect(for {
+    ref <- Ref.make[Option[Int]](None)
+  } yield new Service {
+    override def get(key: String): ZIO[Any, Nothing, Option[Int]] = ref.get
+    override def set(key: String, value: Int): ZIO[Any, Nothing, Unit] = ref.set(Some(value))
   })
 }
--- kata/zio-kata/src/test/scala/dojo/StoreSuite.scala
+++ kata/zio-kata/src/test/scala/dojo/StoreSuite.scala
@@ -10,8 +10,15 @@ import zio.Runtime
 class StoreSuite extends AnyFreeSpecLike with Matchers with Inside {
   "should return None if key doesn't exist" in {
     val prog: ZIO[Store, Nothing, Option[Int]] = Store.get("notExists")
-    val provided: ZIO[Any, Nothing, Option[Int]] = prog.provideLayer(Store.Live)
+    val provided: ZIO[Any, Nothing, Option[Int]] = prog.provideLayer(Store.live)
     val default = Runtime.default
     default.unsafeRun(provided) shouldBe None
+  }
+
+  "should return set value of key" in {
+    val test: ZIO[Store, Nothing, Option[Int]] = Store.set("key", 5) *> Store.get("key")
+    val provided: ZIO[Any, Nothing, Option[Int]] = test.provideLayer(Store.live)
+    val default = Runtime.default
+    default.unsafeRun(provided) shouldBe Some(5)
   }
 }
